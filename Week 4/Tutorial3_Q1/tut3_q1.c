cbec8fa51ea240de53dc0de3a5bc931e1bdcb7a1
PowerPillz <js.beamish1@gmail.com>
2022-03-27 00:20:15 +0800

    Tutorial 3

---
+++ Week 4/Tutorial3_Q1/tut3_q1.c
@@ -0,0 +1,41 @@
+//HIT365 C Programming
+//Tutorial 3 Question 1
+//Set of statements to accomplish each task
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+
+	/*
+	Sum the odd integers between 1 and 99 using a for statement.
+	Assume the integer variables sum and count have been defined.
+	*/
+
+
+
+
+
+
+	/*
+	Print the value 333.546372 in a field width of 15 characters with
+	precisions of 1,2,3,4 and 5.
+	Left justify the output.
+	What are the five values that print?
+	*/
+
+
+
+
+
+
+	/*
+	Calculate the value of 2.5 raised to the power of 4 using the pow function.
+	Print the result with a precision of 2 in a field width of 10 positions.
+	What is the value it prints?	
+	*/
+
+
+
+}
