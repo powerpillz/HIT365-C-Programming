954d2c1f74abfc04ae63fdb1f38a9f3468eb379e
PowerPillz <js.beamish1@gmail.com>
2022-03-27 16:35:33 +0800

    Tutorial 3 Question 4 completed

--- Week 4/Tutorial3_Q4/tut4_q4.c
+++ Week 4/Tutorial3_Q4/tut4_q4.c
@@ -1,7 +1,7 @@
 /*
 HIT365 C Programming
 Tutorial 3 Question 4
-
+Find the smallest of a user defined group of integers
 */
 
 #include <stdio.h>
@@ -10,8 +10,31 @@ Tutorial 3 Question 4
 int main(void)
 {
 
+	int numInt;		//user specified number of integers
+	int count = 1;
+	int smallest = 0;
+	int temp = 0;
 
 
+	printf("How many integers do you want to assess?: \n");
+	scanf_s("%d", &numInt);
+
+	while (count <= numInt) {
+		printf("Enter integer %d of %d\n", count, numInt);
+		scanf_s("%d", &temp);
+
+		if (smallest == 0) {
+			smallest = temp;
+		}
+		else if (temp < smallest) {
+			smallest = temp;
+		}
+
+		count++;
+
+	}
+
+	printf("The smallest integer you entered is %d", smallest);
 
 	return 0;
 }
